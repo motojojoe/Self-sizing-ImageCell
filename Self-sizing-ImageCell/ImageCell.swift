14ae16dbf55c21151a5c78309eb4766c26d961b9
Atthachai Meethong <atthachai.mee@gmail.com>
2017-10-08 23:14:01 +0700

    Create TableView and mockup data with ImageCell

---
+++ Self-sizing-ImageCell/ImageCell.swift
@@ -0,0 +1,26 @@
+//
+//  ImageCell.swift
+//  Self-sizing-ImageCell
+//
+//  Created by Atthachai Meethong on 10/8/17.
+//  Copyright © 2017 Atthachai Meethong. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Self-sizing-ImageCell/ViewController.swift
+++ Self-sizing-ImageCell/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ImageCell", for: indexPath) as! ImageCell
+        cell.photoView.image = #imageLiteral(resourceName: "swimming")
+        return cell
+    }
+}
