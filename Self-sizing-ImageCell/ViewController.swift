5438d6ae0b1dcb3f68d08c2cd0cb0ae626275c02
Atthachai Meethong <atthachai.mee@gmail.com>
2017-10-08 20:04:33 +0700

    Add file project

---
+++ Self-sizing-ImageCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Self-sizing-ImageCell
+//
+//  Created by Atthachai Meethong on 10/8/17.
+//  Copyright © 2017 Atthachai Meethong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
